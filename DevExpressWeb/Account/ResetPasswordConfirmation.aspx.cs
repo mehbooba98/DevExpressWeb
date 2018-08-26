71888ca41f6029de5ba056eabfd564ac4d03c4eb
mehbooba98 <mehboob_a98@yahoo.com>
2018-08-26 19:44:56 -0400

    Add project files.

---
+++ DevExpressWeb/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace DevExpressWeb.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ DevExpressWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevExpressWeb.Startup))]
+namespace DevExpressWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
